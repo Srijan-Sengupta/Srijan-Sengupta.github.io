f3c2caa203042d3d8f71aa22113a2f95a99d6aba
srijansgupta <srijansenguta306@gmail.com>
2020-08-23 09:52:23 +0530

    Innitial Commit

---
+++ resource/index.js
@@ -0,0 +1,9 @@
+function paint_India_Gradle_Desktop() {
+  window.location.href = "https://github.com/Srijan-Sengupta/Paint-India-Gradle-Desktop";
+}
+function paint_India_Simple() {
+  window.location.href = "https://github.com/Srijan-Sengupta/Paint-India-Simple";
+}
+function functionName() {
+
+}
